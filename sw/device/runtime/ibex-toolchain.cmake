18b4522f6cb2bf41c077f99dc3c7648e14de6e29
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-02-07 15:49:09 +0100

    merge cleaned device ans sim cmake flows

---
+++ sw/device/runtime/ibex-toolchain.cmake
@@ -0,0 +1,14 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR riscv32)
+
+set(CMAKE_ASM_COMPILER /usr/bin/clang)
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+
+set(DEFAULT_COMPILE_FLAGS
+  "-O2 --target=riscv32-unkown-elf -march=rv32imc -mabi=ilp32"
+)
+
+set(CMAKE_ASM_FLAGS ${DEFAULT_COMPILE_FLAGS})
+set(CMAKE_C_FLAGS ${DEFAULT_COMPILE_FLAGS})
+set(CMAKE_CXX_FLAGS ${DEFAULT_COMPILE_FLAGS})
