5d4693a936d4f0344f4411e7c9a9eec574f29282
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-02-26 11:30:33 +0100

    move away from externalproject -- fix sim and device building

--- sw/device/runtime/ibex-toolchain.cmake
+++ sw/device/runtime/ibex-toolchain.cmake
@@ -1,6 +1,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR riscv32)
 
+# Do not check compiler, it only works with custom sw runtime
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
 set(CMAKE_ASM_COMPILER /usr/bin/clang)
 set(CMAKE_C_COMPILER /usr/bin/clang)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++)
@@ -12,3 +15,6 @@ set(DEFAULT_COMPILE_FLAGS
 set(CMAKE_ASM_FLAGS ${DEFAULT_COMPILE_FLAGS})
 set(CMAKE_C_FLAGS ${DEFAULT_COMPILE_FLAGS})
 set(CMAKE_CXX_FLAGS ${DEFAULT_COMPILE_FLAGS})
+set(CMAKE_EXE_LINKER_FLAGS "${DEFAULT_COMPILE_FLAGS} -fuse-ld=lld" CACHE STRING "" FORCE)
+
+
