ec06a3d952527e8d357da45c0326fb69cfa33f84
Josse Van Delm <josse.vandelm@gmail.com>
2026-03-03 14:57:25 +0100

    Add generic accelerator class

---
+++ src/main/scala/accelerator/Accelerator.scala
@@ -0,0 +1,44 @@
+package accelerator
+
+import chisel3._
+import core.DecoupledBusIO
+import streamer.{Streamer, AffineAguConfig, StreamerDir}
+import chisel3.util.DecoupledIO
+
+class Accelerator(addrWidth: Int, dataWidth: Int) extends Module {
+  def setupStreamer(
+      config: AffineAguConfig,
+      dir: StreamerDir.Type,
+      tcdmSide: Vec[core.DecoupledBusIO],
+      accSide: DecoupledIO[Vec[UInt]],
+      start: Bool
+  ) = {
+    val s = Module(
+      Streamer(config.nTemporalDims, config.spatialDimSizes, 3, addrWidth, dataWidth)
+    )
+    // All streamers take the same start signal
+    s.io.tcdmReqs <> tcdmSide
+    s.io.start := start
+    // All streamers take their own config for CSR setup
+    s.io.config := config
+    s.io.dir := dir
+    // Streamer --> Acc
+    // If the streamer is a "read streamer", connect write to don't care
+    if (dir == StreamerDir.read) {
+      accSide.valid := s.io.read.valid
+      s.io.read.ready := accSide.ready
+      accSide.bits := s.io.read.bits.asTypeOf(accSide.bits)
+      s.io.write := DontCare
+    }
+    // Acc --> Streamer
+    // If the streamer is a "write streamer", connect read to don't care
+    else {
+      s.io.write.valid := accSide.valid
+      accSide.ready := s.io.write.ready
+      s.io.write.bits := accSide.bits.asTypeOf(s.io.write.bits)
+      s.io.read := DontCare
+    }
+
+    s
+  }
+}
--- src/main/scala/accelerator/AluAccelerator.scala
+++ src/main/scala/accelerator/AluAccelerator.scala
@@ -7,10 +7,11 @@ import csr.CsrIO
 import csr.CsrInterface
 import datapath.AluArray
 import chisel3.util.DecoupledIO
+import accelerator.Accelerator
+import dataclass.data
 
-// DMA instantiates streamer <-> datapath <-> streamer
-//
-class AluAccelerator(addrWidth: Int, dataWidth: Int) extends Module {
+// Accelerator instantiates streamer <-> datapath <-> streamer
+class AluAccelerator(addrWidth: Int, dataWidth: Int) extends Accelerator(addrWidth, dataWidth) {
 
   val parallelUnroll = 4 // Hardcode spatial unrolling
   val numPorts = parallelUnroll
@@ -36,36 +37,9 @@ class AluAccelerator(addrWidth: Int, dataWidth: Int) extends Module {
 
   val csrItf = Module(new CsrInterface((new CsrVals).numRegs, 0x900))
   csrItf.io.csr <> io.csr
+  val csrStart = csrItf.io.start
   val csrVals = VecInit(csrItf.io.vals.reverse).asTypeOf(new CsrVals)
   dontTouch(csrVals)
-
-  // helper function to avoid repeating boilerplate
-  def setupStreamer(config: AffineAguConfig, dir: StreamerDir.Type, target: DecoupledIO[Vec[UInt]]) = {
-    val s = Module(Streamer(config.nTemporalDims, config.spatialDimSizes, 3, addrWidth, dataWidth))
-    // All streamers take the same start signal
-    s.io.start := csrItf.io.start
-    // All streamers take their own config for CSR setup
-    s.io.config := config
-    s.io.dir := dir
-    // Streamer --> Acc
-    // If the streamer is a "read streamer", connect write to don't care
-    if (dir == StreamerDir.read) {
-      target.valid := s.io.read.valid
-      s.io.read.ready := target.ready
-      target.bits := s.io.read.bits.asTypeOf(target.bits)
-      s.io.write := DontCare
-    }
-    // Acc --> Streamer
-    // If the streamer is a "write streamer", connect read to don't care
-    else {
-      s.io.write.valid := target.valid
-      target.ready := s.io.write.ready
-      s.io.write.bits := target.bits.asTypeOf(s.io.write.bits)
-      s.io.read := DontCare
-    }
-
-    s
-  }
 
   val aluArray = Module(new AluArray(parallelUnroll, dataWidth))
   val truncated_sel = csrVals.select.asTypeOf(UInt(2.W))
@@ -73,14 +47,11 @@ class AluAccelerator(addrWidth: Int, dataWidth: Int) extends Module {
 
   val queueDepth = 3
   // Use streamers for tcdm requests
-  val aStreamer = setupStreamer(csrVals.aStreamerConfig, StreamerDir.read, aluArray.io.A_in)
-  aStreamer.io.tcdmReqs <> io.aData
-
-  val bStreamer = setupStreamer(csrVals.bStreamerConfig, StreamerDir.read, aluArray.io.B_in)
-  bStreamer.io.tcdmReqs <> io.bData
-
-  val cStreamer = setupStreamer(csrVals.cStreamerConfig, StreamerDir.write, aluArray.io.C_out)
-  cStreamer.io.tcdmReqs <> io.cData
-
+  val aStreamer =
+    setupStreamer(csrVals.aStreamerConfig, StreamerDir.read, io.aData, aluArray.io.A_in, csrStart)
+  val bStreamer =
+    setupStreamer(csrVals.bStreamerConfig, StreamerDir.read, io.bData, aluArray.io.B_in, csrStart)
+  val cStreamer =
+    setupStreamer(csrVals.cStreamerConfig, StreamerDir.write, io.cData, aluArray.io.C_out, csrStart)
   csrItf.io.done := cStreamer.io.done
 }
