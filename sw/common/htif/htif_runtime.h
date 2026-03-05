7a97d52b8329bf2af68e3c594979fda807c3cafa
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-03-05 17:35:38 +0100

    support c++ in htif runtime

--- sw/common/htif/htif_runtime.h
+++ sw/common/htif/htif_runtime.h
@@ -1,6 +1,10 @@
 #ifndef __HTIF_RUNTIME_H
 #define __HTIF_RUNTIME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Prints a character to simulation output.
 // Implementation buffers characters until the buffer is full
 // or on the first newline ('\n')
@@ -9,5 +13,9 @@ void putchar_(char ch);
 // HTIF exit routine
 // Sends a signal to the simulator host to exit the simulation.
 void htif_exit(int code);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __HTIF_RUNTIME_H
