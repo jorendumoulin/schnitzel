8070ac0e64124638586f01aa98e048bd8f98ecd9
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-03-22 21:49:56 +0100

    add dma lib

---
+++ sw/device/runtime/dma/src/dma.cpp
@@ -0,0 +1,20 @@
+#include "memref.hh"
+
+typedef MemRefDescriptor<uint8_t, 1> memref;
+
+// MLIR-compiled function interfaces:
+extern "C" void _mlir_ciface_memcpy_l3_l1(MemRefDescriptor<uint8_t, 1> *src,
+                                          MemRefDescriptor<uint8_t, 1> *dest);
+
+void *dma_memcpy(void *dest, const void *src, size_t count) {
+
+  MemRefDescriptor<uint8_t, 1> memref_src, memref_dest;
+  memref_src.aligned = (unsigned char *)src;
+  memref_dest.aligned = (unsigned char *)dest;
+  memref_src.sizes[0] = count;
+  memref_dest.sizes[0] = count;
+
+  _mlir_ciface_memcpy_l3_l1(&memref_src, &memref_dest);
+
+  return dest;
+}
