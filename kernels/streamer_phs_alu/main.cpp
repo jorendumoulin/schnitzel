91544061c050890e09cab80fcef8c80d6440a2c2
Josse Van Delm <josse.vandelm@gmail.com>
2026-04-09 13:45:42 +0200

    Autoformat cpp

--- kernels/streamer_phs_alu/main.cpp
+++ kernels/streamer_phs_alu/main.cpp
@@ -1,5 +1,5 @@
-#include <runtime.h>
 #include <memref.hh>
+#include <runtime.h>
 #include <stdalign.h>
 #include <stdint.h>
 
@@ -11,15 +11,15 @@ extern "C" void _mlir_ciface_streamer_add(MemRefDescriptor<int64_t, 1> *A,
                                           MemRefDescriptor<int64_t, 1> *O);
 
 // Test data in AXI/L3 memory (64-byte aligned for 512-bit AXI bus)
-alignas(64) int64_t A[DATA_LEN] = {1, 2, 3, 4, 5, 6, 7, 8,
-                                    9, 10, 11, 12, 13, 14, 15, 16};
-alignas(64) int64_t B[DATA_LEN] = {10, 20, 30, 40, 50, 60, 70, 80,
-                                    90, 100, 110, 120, 130, 140, 150, 160};
+alignas(64) int64_t A[DATA_LEN] = {1, 2,  3,  4,  5,  6,  7,  8,
+                                   9, 10, 11, 12, 13, 14, 15, 16};
+alignas(64) int64_t B[DATA_LEN] = {10, 20,  30,  40,  50,  60,  70,  80,
+                                   90, 100, 110, 120, 130, 140, 150, 160};
 alignas(64) int64_t O[DATA_LEN] = {0};
 
 // Expected: A + B
-int64_t G[DATA_LEN] = {11, 22, 33, 44, 55, 66, 77, 88,
-                        99, 110, 121, 132, 143, 154, 165, 176};
+int64_t G[DATA_LEN] = {11, 22,  33,  44,  55,  66,  77,  88,
+                       99, 110, 121, 132, 143, 154, 165, 176};
 
 int main() {
   int hart = hart_id();
