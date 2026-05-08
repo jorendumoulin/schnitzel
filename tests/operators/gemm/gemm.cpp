047d9f733e56ef00ca309afbe2c0ac1d901e5ecd
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-05-08 17:34:08 +0200

    make gemm test compilable

--- tests/operators/gemm/gemm.cpp
+++ tests/operators/gemm/gemm.cpp
@@ -1,4 +1,6 @@
 #include <memref.hh>
+#include <runtime.h>
+#include <stdint.h>
 
 // Testing code to just call the compiled gemm operator
 //
@@ -6,31 +8,36 @@ extern "C" void _mlir_ciface_gemm(MemRefDescriptor<int32_t, 2> result,
                                   MemRefDescriptor<int8_t, 2> a,
                                   MemRefDescriptor<int8_t, 2> b);
 
-#ifndef M
-#define M 16
+#ifndef GEMM_M
+#define GEMM_M 16
 #endif
 
-#ifndef N
-#define N 16
+#ifndef GEMM_N
+#define GEMM_N 16
 #endif
 
-#ifndef K
-#define K 16
+#ifndef GEMM_K
+#define GEMM_K 16
 #endif
 
-int8_t a_data[M * K];
-MemRefDescriptor<int8_t, 2> a = {a_data, a_data, 0, {M, K}, {K, 1}};
+int8_t a_data[GEMM_M * GEMM_K];
+MemRefDescriptor<int8_t, 2> a = {
+    a_data, a_data, 0, {GEMM_M, GEMM_K}, {GEMM_K, 1}};
 
-int8_t b_data[K * N];
-MemRefDescriptor<int8_t, 2> b = {b_data, b_data, 0, {K, N}, {N, 1}};
+int8_t b_data[GEMM_K * GEMM_N];
+MemRefDescriptor<int8_t, 2> b = {
+    b_data, b_data, 0, {GEMM_K, GEMM_N}, {GEMM_N, 1}};
 
-int32_t result_data[M * N];
+int32_t result_data[GEMM_M * GEMM_N];
 MemRefDescriptor<int32_t, 2> result = {
-    result_data, result_data, 0, {M, N}, {N, 1}};
+    result_data, result_data, 0, {GEMM_M, GEMM_N}, {GEMM_N, 1}};
 
 // Variables to reference from python:
 extern "C" int8_t *gemm_a_data = a.aligned;
 extern "C" int8_t *gemm_b_data = b.aligned;
 extern "C" int32_t *gemm_d_data = result.aligned;
 
-int main() { _mlir_ciface_gemm(result, a, b); }
+int main() {
+  _mlir_ciface_gemm(result, a, b);
+  htif_exit(0);
+}
