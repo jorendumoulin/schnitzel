0eab27582f00d165f1d64e7babbbecf8cd759375
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-05-13 17:41:37 +0200

    add support for higher bitwidth tcdm memory banks with memwidth converters

--- src/main/scala/cluster/TensorCluster.scala
+++ src/main/scala/cluster/TensorCluster.scala
@@ -17,11 +17,12 @@ import accelerator.TensorCore
 import csr.CsrIO
 import config.ClusterConfig
 import config.MemoryConfig
+import memory.MemWidthConverter
 
 class TensorCluster extends Module {
 
   val wideAxiDataWidth = 512
-  val tcdmDataWidth = 32
+  val tcdmDataWidth = 64
 
   val io = IO(new Bundle {
     val axi = new AXIBundle(AXIConfig(idWidth = 6, dataWidth = wideAxiDataWidth))
@@ -35,6 +36,8 @@ class TensorCluster extends Module {
   // Split data interface into AXI <> TCDM
   val memMux_0 = Module(new MemDemux(CoreConfig.addrWidth, CoreConfig.dataWidth, 0x10000000));
   memMux_0.io.in <> core_0.io.dmem
+  val memWidthConverter_0 = Module(new MemWidthConverter(CoreConfig.addrWidth, 32, tcdmDataWidth))
+  memWidthConverter_0.io.in <> memMux_0.io.outs(1)
 
   // Convert data interface to AXI
   val mem_to_axi_0 = Module(
@@ -57,6 +60,8 @@ class TensorCluster extends Module {
   // Split data interface into AXI <> TCDM
   val memMux_1 = Module(new MemDemux(CoreConfig.addrWidth, CoreConfig.dataWidth, 0x10000000));
   memMux_1.io.in <> core_1.io.dmem
+  val memWidthConverter_1 = Module(new MemWidthConverter(CoreConfig.addrWidth, 32, tcdmDataWidth))
+  memWidthConverter_1.io.in <> memMux_1.io.outs(1)
 
   // Convert data interface to AXI
   val mem_to_axi_1 = Module(
@@ -89,13 +94,13 @@ class TensorCluster extends Module {
   val accPorts = tensorCore.io.aData ++ tensorCore.io.bData ++ tensorCore.io.cData
 
   // TCDM
-  val numBanks = 32
+  val numBanks = 64
   val tcdm_sram = VecInit(Seq.fill(numBanks)(SRAM.masked(1024, Vec(tcdmDataWidth / 8, UInt(8.W)), 0, 0, 1)));
   val tcdm_ports = VecInit(tcdm_sram.map(sram => sram.readwritePorts(0)));
 
-  val interconnect = Module(new Interconnect(42, numBanks, CoreConfig.addrWidth, tcdmDataWidth));
+  val interconnect = Module(new Interconnect(26, numBanks, CoreConfig.addrWidth, tcdmDataWidth));
 
-  interconnect.io.ins <> VecInit(Seq(memMux_0.io.outs(1), memMux_1.io.outs(1))) ++ dma.io.data ++ accPorts
+  interconnect.io.ins <> VecInit(Seq(memWidthConverter_0.io.out, memWidthConverter_1.io.out)) ++ dma.io.data ++ accPorts
 
   interconnect.io.outs.zip(tcdm_ports).foreach { case (out, port) =>
     port.enable := out.req.valid
