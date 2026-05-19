4905a8be65c6b989065ddf9d908bcc3dec38ba18
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-05-19 14:26:33 +0200

    add top module selection in emitverilog functionality

--- src/main/scala/sim/EmitVerilog.scala
+++ src/main/scala/sim/EmitVerilog.scala
@@ -2,7 +2,8 @@ package sim
 
 import chisel3._
 import circt.stage.ChiselStage
-import top.Top
+import top.{Top, TensorTop}
+import config.SystemConfig
 import upickle.default.write
 import java.io.File
 import java.io.PrintWriter
@@ -10,40 +11,43 @@ import java.io.PrintWriter
 /** Elaboration object to generate Verilog for spitchel integration
   *
   * This generates the Verilog RTL that will be verilated and integrated with the spike/fesvr infrastructure.
+  *
+  * Pass `--top=<name>` to select which top-level module to elaborate. Extend the match below to add new
+  * configurations.
   */
 object EmitVerilog extends App {
 
-  println("Generating Verilog for Schnitzel Core...")
+  val topName: String = args
+    .collectFirst { case s if s.startsWith("--top=") => s.stripPrefix("--top=") }
+    .getOrElse("tensor")
 
-  // Generate Verilog to the generated/ directory
+  println(s"Generating Verilog for Schnitzel Core (top=$topName)...")
+
   val outputDir = "generated"
 
-  var topModule: Top = null
+  var topConfig: SystemConfig = null
 
   ChiselStage.emitSystemVerilogFile(
-    {
-      topModule = new Top
-      topModule
+    topName match {
+      case "tensor"  => val m = new TensorTop; topConfig = m.getConfig; m
+      case "default" => val m = new Top; topConfig = m.getConfig; m
+      case other     => sys.error(s"Unknown --top value '$other'.")
     },
     args = Array("--target-dir", outputDir),
     firtoolOpts = Array(
       "-disable-all-randomization",
       "-strip-debug-info",
-      // inline verilog verification
       "--enable-layers=Verification,Verification.Assert,Verification.Assume,Verification.Cover"
     )
   )
-  val configJson = write(topModule.getConfig, indent = 2)
-
-  val dir = new File(outputDir)
 
   val pw = new PrintWriter(new File(s"$outputDir/config.json"))
   try {
-    pw.write(configJson)
+    pw.write(write(topConfig, indent = 2))
     println(s"Successfully generated metadata at $outputDir/config.json")
   } finally {
     pw.close()
   }
 
-  println(s"Verilog generated in $outputDir/Top.sv")
+  println(s"Verilog generated in $outputDir/")
 }
