5750a75aa51d1efaa2832c05dd36f62d923e902b
Joren Dumoulin <joren.dumoulin@kuleuven.be>
2026-05-19 10:39:57 +0200

    add core subsystem helper

--- src/main/scala/cluster/TensorCluster.scala
+++ src/main/scala/cluster/TensorCluster.scala
@@ -3,21 +3,17 @@ package cluster
 import chisel3._
 
 import icache.InstructionCache
-import core.{Core, DecoupledBusIO, CoreConfig}
-import axi.{AXIBundle, AXIConfig, AXIMux, DecoupledIOToAXI}
+import core.CoreConfig
+import axi.{AXIBundle, AXIConfig, AXIMux}
 import interconnect.Interconnect
 import chisel3.util.SRAM
-import memory.MemDemux
 import csr.HWBarrier
 import dma.Dma
-import csr.{CsrDemux, CsrCombiner, CsrOp, CsrReq}
-import chisel3.util.Decoupled
-import icache.InstructionCache
+import csr.CsrCombiner
 import accelerator.TensorCore
 import csr.CsrIO
 import config.ClusterConfig
 import config.MemoryConfig
-import memory.MemWidthConverter
 
 class TensorCluster extends Module {
 
@@ -29,67 +25,42 @@ class TensorCluster extends Module {
     val csr = new CsrIO()
   })
 
-  // Define the first RISC-V Core:
-  // RISC-V Core
-  val core_0 = Module(new Core(2))
+  val wideAxiCfg = AXIConfig(dataWidth = wideAxiDataWidth)
 
-  // Split data interface into AXI <> TCDM
-  val memMux_0 = Module(new MemDemux(CoreConfig.addrWidth, CoreConfig.dataWidth, 0x10000000));
-  memMux_0.io.in <> core_0.io.dmem
-  val memWidthConverter_0 = Module(new MemWidthConverter(CoreConfig.addrWidth, 32, tcdmDataWidth))
-  memWidthConverter_0.io.in <> memMux_0.io.outs(1)
-
-  // Convert data interface to AXI
-  val mem_to_axi_0 = Module(
-    new DecoupledIOToAXI(CoreConfig.addrWidth, CoreConfig.dataWidth, AXIConfig(dataWidth = wideAxiDataWidth), 1)
-  )
-  memMux_0.io.outs(0) <> mem_to_axi_0.io.bus
-
-  val csrDemux_0 = Module(new CsrDemux(3, Seq((0x800L, 0x10L), (0x810L, 0x10L))))
-  csrDemux_0.io.in <> core_0.io.csr
-
-  // Attach dma to first core:
-  val dma = Module(
-    new Dma(addrWidth = CoreConfig.addrWidth, dataWidth = tcdmDataWidth, AXIConfig(dataWidth = wideAxiDataWidth), 3)
-  )
-  dma.io.csr <> csrDemux_0.io.outs(2)
-
-  // Second core:
-  val core_1 = Module(new Core(1))
-
-  // Split data interface into AXI <> TCDM
-  val memMux_1 = Module(new MemDemux(CoreConfig.addrWidth, CoreConfig.dataWidth, 0x10000000));
-  memMux_1.io.in <> core_1.io.dmem
-  val memWidthConverter_1 = Module(new MemWidthConverter(CoreConfig.addrWidth, 32, tcdmDataWidth))
-  memWidthConverter_1.io.in <> memMux_1.io.outs(1)
-
-  // Convert data interface to AXI
-  val mem_to_axi_1 = Module(
-    new DecoupledIOToAXI(CoreConfig.addrWidth, CoreConfig.dataWidth, AXIConfig(dataWidth = wideAxiDataWidth), 2)
-  )
-  memMux_1.io.outs(0) <> mem_to_axi_1.io.bus
-
-  val csrDemux_1 = Module(new CsrDemux(3, Seq((0x800L, 0x10L), (0x810L, 0x10L))))
-  csrDemux_1.io.in <> core_1.io.csr
-
-  // Attach accelerator to second core:
+  // Accelerators -- created up front so each core's CSR demux can attach to them.
+  val dma = Module(new Dma(addrWidth = CoreConfig.addrWidth, dataWidth = tcdmDataWidth, wideAxiCfg, 3))
   val tensorCore = Module(
     new TensorCore(addrWidth = CoreConfig.addrWidth, dataWidth = tcdmDataWidth, M = 8, N = 8, K = 8)
   )
-  tensorCore.io.csr <> csrDemux_1.io.outs(2)
+
+  // Per-core subsystems (Core + mem split + width converter + AXI adapter + CSR demux).
+  val core_0 = CoreSubsystem(
+    hartId = 2,
+    accelerator = dma.io.csr,
+    axiId = 1,
+    tcdmDataWidth = tcdmDataWidth,
+    wideAxiCfg = wideAxiCfg
+  )
+  val core_1 = CoreSubsystem(
+    hartId = 1,
+    accelerator = tensorCore.io.csr,
+    axiId = 2,
+    tcdmDataWidth = tcdmDataWidth,
+    wideAxiCfg = wideAxiCfg
+  )
 
   // Global synchronization CSR (0x800) - coupled and sent externally
   val csrCombiner = Module(new CsrCombiner(2))
-  csrCombiner.io.ins(0) <> csrDemux_0.io.outs(0)
-  csrCombiner.io.ins(1) <> csrDemux_1.io.outs(0)
+  csrCombiner.io.ins(0) <> core_0.globalCsr
+  csrCombiner.io.ins(1) <> core_1.globalCsr
   csrCombiner.io.out <> io.csr
 
   // Cluster hw barrier (local synchronization 0x810)
-  val barrier = Module(new HWBarrier(2));
-  barrier.io.ins <> Seq(csrDemux_0.io.outs(1), csrDemux_1.io.outs(1))
+  val barrier = Module(new HWBarrier(2))
+  barrier.io.ins <> Seq(core_0.localCsr, core_1.localCsr)
 
   // Instruction Cache
-  val icache = InstructionCache(Seq(core_0.io.imem, core_1.io.imem))
+  val icache = InstructionCache(Seq(core_0.imem, core_1.imem))
 
   // Accelerator ports:
   val accPorts = tensorCore.io.aData ++ tensorCore.io.bData ++ tensorCore.io.cData
@@ -100,13 +71,13 @@ class TensorCluster extends Module {
   val tcdm_ports = VecInit(tcdm_sram.map(sram => sram.readwritePorts(0)));
 
   Interconnect(
-    inputs = Seq(memWidthConverter_0.io.out, memWidthConverter_1.io.out) ++ dma.io.data ++ accPorts,
+    inputs = Seq(core_0.tcdm, core_1.tcdm) ++ dma.io.data ++ accPorts,
     outputs = tcdm_ports
   )
 
   // AXI Crossbar
   AXIMux(
-    inputs = Seq(icache.io.axi, mem_to_axi_0.io.axi, mem_to_axi_1.io.axi, dma.io.axi),
+    inputs = Seq(icache.io.axi, core_0.axi, core_1.axi, dma.io.axi),
     output = io.axi
   )
 
